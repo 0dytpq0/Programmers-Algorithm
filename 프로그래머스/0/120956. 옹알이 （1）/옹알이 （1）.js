c967c819fc4072d7a1789230b0b799c1deb4fd5d
Joseph Park <81671404+0dytpq0@users.noreply.github.com>
2024-05-17 15:09:48 +0900

    [level 0] Title: 옹알이 (1), Time: 6.83 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+    let answer = 0;
+    const canSpeak = ['aya','ye','woo','ma']
+    
+    babbling.forEach((babblingWord,idx) => {
+        let curBabblingWord = babblingWord
+        canSpeak.forEach((canSpeakWord) => {
+            curBabblingWord.includes(canSpeakWord) ? curBabblingWord = curBabblingWord.replace(canSpeakWord,' ') : null
+        })
+        curBabblingWord.trim().length === 0 ? answer += 1 : null
+        console.log(answer)
+    })
+    
+    console.log(babbling)
+    
+    
+    return answer;
+}
