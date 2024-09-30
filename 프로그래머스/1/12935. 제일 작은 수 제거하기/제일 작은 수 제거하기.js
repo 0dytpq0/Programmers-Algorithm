863d2d48e37b0b4d61f4f496be8a69fcdea24264
Joseph Park <81671404+0dytpq0@users.noreply.github.com>
2024-09-30 14:32:00 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.77 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -4,6 +4,7 @@ function solution(arr) {
     
     arr.splice(arr.indexOf(Math.min(...arr)),1);
     arr.length <= 0 ? answer = [-1] : answer = arr
-    
+    //     arr.sort((a, b) => b - a);
+    // arr.pop();
     return answer
 }
