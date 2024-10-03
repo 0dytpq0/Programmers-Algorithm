e566d5f29b2087fd9766b4279148a4e848ab052d
Joseph Park <81671404+0dytpq0@users.noreply.github.com>
2024-10-03 15:41:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 373.49 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,18 +1,12 @@
-// 
-
 function solution(elements) {
-    const sums = new Set()
-    const circularArr = elements.concat(elements)
-   
-    for(let length = 1; length <= elements.length; length++){
-       for(let start = 0; start < elements.length; start++){
-           let sum = 0;
-           for(let i = start; i < start + length; i++){
-               sum += circularArr[i]
-           }
-           
-           sums.add(sum)
-       }
+    const circular = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            set.add(sum);
+        }
     }
-    return sums.size;
+    return set.size;
 }
