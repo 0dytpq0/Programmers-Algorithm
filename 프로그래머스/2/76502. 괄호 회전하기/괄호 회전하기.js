734ef6ea4a463e4a64cbb2dfffb8173f6ccd44d2
Joseph Park <81671404+0dytpq0@users.noreply.github.com>
2024-10-03 16:18:17 +0900

    [level 2] Title: 괄호 회전하기, Time: 137.43 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,25 @@
+function solution(s) {
+    let answer = 0;
+    const arr = s.split('')
+    const currr = {'}' : '{', ']' : '[', ')' : '(' }
+    let stk = []
+    for(let i = 0; i < arr.length; i++){
+        for(let j =0; j< arr.length; j++){
+            if(stk.length > 0 && stk.includes(currr[arr[j]])){
+                stk.pop()
+            } else{
+                stk.push(arr[j])
+            }
+        }
+        if(stk.length === 0){
+            answer++
+        }
+        stk = []
+        arr.push(arr[0])
+        arr.shift()
+        
+    }
+    
+    
+    return answer;
+}
