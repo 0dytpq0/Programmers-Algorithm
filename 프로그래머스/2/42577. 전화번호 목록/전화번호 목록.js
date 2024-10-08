2edee33bc64991c797834fba4240f7688fa9dd83
Joseph Park <81671404+0dytpq0@users.noreply.github.com>
2024-10-09 08:22:50 +0900

    [level 2] Title: 전화번호 목록, Time: 429.82 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -4,7 +4,8 @@ function solution(phone_book) {
     
     for(let phone of phone_book){
         for(let i = 1; i < phone.length; i++){
-            if(phoneMap.has(phone.slice(0,i))) answer =false
+            if(phoneMap.has(phone.slice(0,i))) answer = false
+            
         }
     }
     
